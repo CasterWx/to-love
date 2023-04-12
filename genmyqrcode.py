e0a6259112ab116f5e6f3aa1ce5e74f7c3cf9ed2
AntzUhl <1325200471@qq.com>
2023-04-12 23:14:03 +0800

    init
    init

---
+++ genmyqrcode.py
@@ -0,0 +1,17 @@
+import os
+from MyQR import myqr
+version, level, qr_name = myqr.run(
+    'https://casterwx.github.io/to-lover/',
+    version=1,
+    level='H',
+    picture='pic-icon.png',
+    colorized=True,
+    contrast=1.0,
+    brightness=1.0,
+    save_name='ip_jackLu.png',
+    save_dir=os.getcwd()
+)
+print('version', version)
+print('level', level)
+print('qr_name', qr_name)
+# os.system('pause')
